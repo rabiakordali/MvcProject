035667cc3e5cf82d6b835784e1c50602b60f23b0
b rabia kordali <rabiakordali@hotmail.com>
2024-10-14 03:25:51 +0300

    Entity Layer'de sınıflara [Key] ve ICollection ilişkileri eklendi. Bu güncellemeler sayesinde, veritabanı tasarımında ilişkilerin daha iyi yönetilmesini sağlanacak.

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer.Concrete
+namespace EntityLayer.Concreate
 {
     public class Contact
     {
+
+        [Key]
+        public int ContactId { get; set; }
+
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        public string UserMail { get; set; }
+
+        [StringLength(50)]
+        public string Subject { get; set; }
+
+        [StringLength(500)]
+        public string Message { get; set; }
+        
+
     }
 }
--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer.Concrete
+namespace EntityLayer.Concreate
 {
     public class Heading
     {
+        [Key]
+        public int HeadingID { get; set; }
+
+        [StringLength(100)]
+        public string HeadingName { get; set; }
+       
+
+        public DateTime HeadingTime { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public ICollection<Content> Contets { get; set; }
+        ///
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
     }
 }
