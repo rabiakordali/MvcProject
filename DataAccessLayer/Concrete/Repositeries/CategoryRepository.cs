2d7866f073adc707acde497955c898e766c2e5f2
b rabia kordali <rabiakordali@hotmail.com>
2024-10-17 17:28:20 +0300

    Repository Sınıfı Ve Metotların İçinin Doldurulması

---
+++ DataAccessLayer/Concrete/Repositeries/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositeries
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c=new Context();
+        DbSet<Category> _object;
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
